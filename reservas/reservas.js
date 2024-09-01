c28900d1c863ee419f5c59800578d569d382750a
felig76 <blakyn19@gmail.com>
2024-09-01 19:24:28 -0300

    Guardado de horarios de reservas en const y acomodo del for

--- reservas/reservas.js
+++ reservas/reservas.js
@@ -9,6 +9,7 @@ function obtenerSieteDias() {
     }
     return days;
 }
+const HORAS_TURNOS = ['08:00', '11:00', '14:00', '17:00', '20:00'];
 
 document.getElementById('selectCancha').addEventListener('change', function() {
   var valorSeleccionado = this.value;
@@ -16,6 +17,7 @@ document.getElementById('selectCancha').addEventListener('change', function() {
 });
 
 function mostrarOpcionesReservas(numeroCancha) {
+  // Determinar elemento html y guardar en una variable
   var contenedorReservas = document.getElementById('listasReservasDiarias');
   // Limpia el contenedor antes de agregar nuevas listas
   contenedorReservas.innerHTML = '';
@@ -24,7 +26,7 @@ function mostrarOpcionesReservas(numeroCancha) {
   const dias = obtenerSieteDias();
 
   if (numeroCancha != 0) {
-  
+    // Obtener registros de reservas usando el código php y transformarlo a formato json
     fetch('reservas.php').then(response => response.json()).then(reservas => {
 
       for (let i = 0; i < dias.length; i++) {
@@ -32,7 +34,7 @@ function mostrarOpcionesReservas(numeroCancha) {
         var diaContenedor = document.createElement('div');
         diaContenedor.className = 'dia-semana';
         
-        // Crear un encabezado para el día
+        // Crear un encabezado con la fecha para el día
         var encabezadoDia = document.createElement('h3');
         encabezadoDia.textContent = dias[i];
         diaContenedor.appendChild(encabezadoDia);
@@ -41,13 +43,25 @@ function mostrarOpcionesReservas(numeroCancha) {
         var listaReservas = document.createElement('ul');
         
         // Agregar elementos a la lista de reservas
-        for (let j = 0; j < 3; j++) {
+        for (let j = 0; j < HORAS_TURNOS.length; j++) {
+          var hora = HORAS_TURNOS[j];
+          var reserva = reservas.find(r => 
+            r.fecha_reserva === dias[i] &&
+            r.hora_inicio === hora &&
+            r.cancha == numeroCancha
+          );
+
           var nuevaReserva = document.createElement('li');
-          nuevaReserva.textContent = "Cancha " + numeroCancha + ", horario " + (j+1) + ", día " + dias[i];
+          nuevaReserva.textContent = `Cancha ${numeroCancha}, horario ${hora}, día ${dias[i]}`;
           nuevaReserva.classList.add('reserva-item');
-          
+          if (reserva) {
+            nuevaReserva.classList.add('reservado');
+            nuevaReserva.textContent += ' - Reservado';
+          } else {
+            nuevaReserva.classList.add('disponible');
+          }
           nuevaReserva.setAttribute('data-cancha', numeroCancha);
-          nuevaReserva.setAttribute('data-horario', j+1);
+          nuevaReserva.setAttribute('data-horario', hora);
           nuevaReserva.setAttribute('data-dia', dias[i]);
 
           listaReservas.appendChild(nuevaReserva);
