ee26ef2bd46e98b38c574570112ef40689cfa5d0
Zarpitas <142760252+Zarpitas@users.noreply.github.com>
2024-09-02 09:44:28 -0300

    Update reservas.js

--- reservas/reservas.js
+++ reservas/reservas.js
@@ -1,18 +1,25 @@
 function obtenerSieteDias() {
   const days = [];
   const today = new Date();
-  const formatter = new Intl.DateTimeFormat('es-ES', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
-    for (let i = 0; i < 7; i++) {
-      let day = new Date(today);
-      day.setDate(today.getDate() + i);
-      days.push(formatter.format(day));
-    }
-    return days;
+  
+  for (let i = 0; i < 7; i++) {
+    let day = new Date(today);
+    day.setDate(today.getDate() + i);
+    // Obtener el mes y el día
+    const month = String(day.getMonth() + 1).padStart(2, '0'); // Mes en formato de dos dígitos
+    const date = String(day.getDate()).padStart(2, '0'); // Día en formato de dos dígitos
+    // Crear la fecha en formato MM-DD
+    const formattedDate = `${month}-${date}`;
+    days.push(formattedDate);
+  }
+  
+  return days;
 }
 const HORAS_TURNOS = ['08:00', '11:00', '14:00', '17:00', '20:00'];
 
 document.getElementById('selectCancha').addEventListener('change', function() {
   var valorSeleccionado = this.value;
+  console.log("cambio de cancha detectado");
   mostrarOpcionesReservas(valorSeleccionado);
 });
 
@@ -27,7 +34,7 @@ function mostrarOpcionesReservas(numeroCancha) {
 
   if (numeroCancha != 0) {
     // Obtener registros de reservas usando el código php y transformarlo a formato json
-    fetch('reservas.php').then(response => response.json()).then(reservas => {
+    /*fetch('reservas.php').then(response => response.json()).then(reservas => {*/
 
       for (let i = 0; i < dias.length; i++) {
         // Crear un nuevo contenedor para cada día
@@ -44,34 +51,106 @@ function mostrarOpcionesReservas(numeroCancha) {
         
         // Agregar elementos a la lista de reservas
         for (let j = 0; j < HORAS_TURNOS.length; j++) {
-          var hora = HORAS_TURNOS[j];
-          var reserva = reservas.find(r => 
+          var estaReservado = reservasHechas.find(r => 
             r.fecha_reserva === dias[i] &&
-            r.hora_inicio === hora &&
-            r.cancha == numeroCancha
+            r.hora_inicio === HORAS_TURNOS[j] &&
+            r.cancha === numeroCancha
           );
-
-          var nuevaReserva = document.createElement('li');
-          nuevaReserva.textContent = `Cancha ${numeroCancha}, horario ${hora}, día ${dias[i]}`;
-          nuevaReserva.classList.add('reserva-item');
-          if (reserva) {
-            nuevaReserva.classList.add('reservado');
-            nuevaReserva.textContent += ' - Reservado';
+          console.log(estaReservado);
+          var nuevaTarjetaReserva = document.createElement('li');
+          nuevaTarjetaReserva.textContent = `Cancha ${numeroCancha}, horario ${HORAS_TURNOS[j]}, día ${dias[i]}`;
+          nuevaTarjetaReserva.classList.add('reserva-item');
+          if (estaReservado) {
+            nuevaTarjetaReserva.classList.add('reservado');
+            nuevaTarjetaReserva.textContent += ' - Reservado';
           } else {
-            nuevaReserva.classList.add('disponible');
+            nuevaTarjetaReserva.classList.add('disponible');
           }
-          nuevaReserva.setAttribute('data-cancha', numeroCancha);
-          nuevaReserva.setAttribute('data-horario', hora);
-          nuevaReserva.setAttribute('data-dia', dias[i]);
+          nuevaTarjetaReserva.setAttribute('data-cancha', numeroCancha);
+          nuevaTarjetaReserva.setAttribute('data-horario', HORAS_TURNOS[j]);
+          nuevaTarjetaReserva.setAttribute('data-dia', dias[i]);
 
-          listaReservas.appendChild(nuevaReserva);
+          listaReservas.appendChild(nuevaTarjetaReserva);
         }
         
         diaContenedor.appendChild(listaReservas);
         contenedorReservas.appendChild(diaContenedor);
       }
 
-    })
+    /*})*/
 
   }
 }
+  
+let reservasHechas = [
+  {
+      "id": 1,
+      "fecha_reserva": "08-25",
+      "hora_inicio": "10:00",
+      "cancha": 1,
+      "usuario_id": 1
+  },
+  {
+      "id": 2,
+      "fecha_reserva": "08-25",
+      "hora_inicio": "13:00",
+      "cancha": 7,
+      "usuario_id": 2
+  },
+  {
+      "id": 3,
+      "fecha_reserva": "08-25",
+      "hora_inicio": "16:00",
+      "cancha": 2,
+      "usuario_id": null
+  },
+  {
+      "id": 4,
+      "fecha_reserva": "08-26",
+      "hora_inicio": "09:00",
+      "cancha": 7,
+      "usuario_id": null
+  },
+  {
+      "id": 5,
+      "fecha_reserva": "08-26",
+      "hora_inicio": "12:00",
+      "cancha": 3,
+      "usuario_id": 3
+  },
+  {
+      "id": 6,
+      "fecha_reserva": "08-26",
+      "hora_inicio": "15:00",
+      "cancha": 7,
+      "usuario_id": 4
+  },
+  {
+      "id": 7,
+      "fecha_reserva": "08-27",
+      "hora_inicio": "08:00",
+      "cancha": 4,
+      "usuario_id": null
+  },
+  {
+      "id": 8,
+      "fecha_reserva": "08-27",
+      "hora_inicio": "11:00",
+      "cancha": 7,
+      "usuario_id": null
+  },
+  {
+      "id": 9,
+      "fecha_reserva": "08-27",
+      "hora_inicio": "14:00",
+      "cancha": 5,
+      "usuario_id": 5
+  },
+  {
+      "id": 10,
+      "fecha_reserva": "08-27",
+      "hora_inicio": "17:00",
+      "cancha": 6,
+      "usuario_id": 6
+  }
+];
