df2ba22b3af455667725026a13a74a673c5e21ba
Zarpitas <142760252+Zarpitas@users.noreply.github.com>
2024-09-10 11:20:12 -0300

    Update reservas.js

--- reservas/reservas.js
+++ reservas/reservas.js
@@ -1,6 +1,4 @@
-//import reservasLocal from "./mocks/registrosReservasLocal.json"
-
-const HORAS_TURNOS = ['08:00', '11:00', '14:00', '17:00', '20:00'];
+const HORAS_TURNOS = ['08:00:00', '11:00:00', '14:00:00', '17:00:00', '20:00:00'];
 
 function obtenerSieteDias() {
   const days = [];
@@ -9,11 +7,12 @@ function obtenerSieteDias() {
   for (let i = 0; i < 7; i++) {
     let day = new Date(today);
     day.setDate(today.getDate() + i);
-    // Obtener el mes y el día
+    // Obtener el año, mes y día
+    const year = day.getFullYear();
     const month = String(day.getMonth() + 1).padStart(2, '0'); // Mes en formato de dos dígitos
     const date = String(day.getDate()).padStart(2, '0'); // Día en formato de dos dígitos
-    // Crear la fecha en formato MM-DD
-    const formattedDate = `${month}-${date}`;
+    // Crear la fecha en formato YYYY-MM-DD
+    const formattedDate = `${year}-${month}-${date}`;
     days.push(formattedDate);
   }
   
@@ -22,14 +21,12 @@ function obtenerSieteDias() {
 
 document.getElementById('selectCancha').addEventListener('change', function() {
   var valorSeleccionado = this.value;
-  console.log("cambio de cancha detectado");
   mostrarOpcionesReservas(valorSeleccionado);
 });
 
 function mostrarOpcionesReservas(numeroCancha) {
-  // Determinar elemento html y guardar en una variable
+  numeroCancha = Number(numeroCancha);
   var contenedorReservas = document.getElementById('listasReservasDiarias');
-  // Limpia el contenedor antes de agregar nuevas listas
   contenedorReservas.innerHTML = '';
 
   // Obtener los próximos 7 días
@@ -55,6 +52,7 @@ function mostrarOpcionesReservas(numeroCancha) {
         
         // Agregar elementos a la lista de reservas
         for (let j = 0; j < HORAS_TURNOS.length; j++) {
+
           var estaReservado = reservas.find(r => 
             r.fecha_reserva === dias[i] &&
             r.hora_inicio === HORAS_TURNOS[j] &&
