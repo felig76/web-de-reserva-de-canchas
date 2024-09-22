d410285281831448d3c9092909a19d450144c7a5
felig76 <blakyn19@gmail.com>
2024-09-22 17:12:08 -0300

    Renombrar php de obtención de reservas hechas

---
+++ reservas/obtenerReservas.php
@@ -0,0 +1,47 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+header('Content-Type: application/json');
+
+// Configuración para base de datos local con XAMPP
+$servername = "localhost";
+$username = "root";
+$password = ""; // XAMPP no tiene contraseña para MySQL por defecto
+$dbname = "reservas_paintball";
+// Configuración para base de datos hosteada
+/*
+$servername = "sql111.infinityfree.com";
+$username = "if0_37225004";
+$password = "6dQGvZ15Uh ";
+$dbname = "if0_37225004_ReservasEzeizaPaintball";
+*/
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    throw new Exception("Conexión fallida: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Reservas";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $reservas = [];
+
+    while($row = $result->fetch_assoc()) {
+        // Convertir los valores numéricos a enteros
+        $row['id'] = (int)$row['id'];
+        $row['cancha'] = (int)$row['cancha'];
+        $row['usuario_id'] = (int)$row['usuario_id'];
+
+        $reservas[] = $row;
+    }
+
+    echo json_encode($reservas);
+} else {
+    echo json_encode([]);
+}
+
+$conn->close();
+?>
